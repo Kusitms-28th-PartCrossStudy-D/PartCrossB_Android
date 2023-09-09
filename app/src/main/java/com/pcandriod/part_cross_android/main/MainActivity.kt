aa89dff8486151fa1a4732bea8b5edc58fa26fd2
arinming <jyandysys1@naver.com>
2023-09-09 16:08:24 +0900

    [feat] : 메인 페이지 리사이클러뷰 구현 완료

--- app/src/main/java/com/pcandriod/part_cross_android/main/MainActivity.kt
+++ app/src/main/java/com/pcandriod/part_cross_android/main/MainActivity.kt
@@ -2,6 +2,8 @@ package com.pcandriod.part_cross_android.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.pcandriod.part_cross_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +15,30 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        initView()
+    }
+
+
+    fun initView() {
+        val itemPost = ArrayList<PostItem>()
+
+        itemPost.add(PostItem("제목1", "내용1"))
+        itemPost.add(PostItem("제목2", "내용2"))
+        itemPost.add(PostItem("제목3", "내용3"))
+        itemPost.add(PostItem("제목4", "내용4"))
+        itemPost.add(PostItem("제목5", "내용5"))
+        itemPost.add(PostItem("제목6", "내용6"))
+        itemPost.add(PostItem("제목7", "내용7"))
+        itemPost.add(PostItem("제목8", "내용8"))
+        itemPost.add(PostItem("제목9", "내용9"))
+        itemPost.add(PostItem("제목10", "내용10"))
+
+
+        val postAdapter = PostAdapter(itemPost)
+        postAdapter.notifyDataSetChanged()
+
+        binding.rvPostList.adapter = postAdapter
+
     }
 }
