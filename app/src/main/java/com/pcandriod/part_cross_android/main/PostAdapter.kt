01ed5a069f47effc9c62b6c9cdf71eafebb4fd7b
arinming <jyandysys1@naver.com>
2023-09-09 15:55:19 +0900

    [feat] : main UI 툴 구축, 리사이클러뷰 어댑터 생성

---
+++ app/src/main/java/com/pcandriod/part_cross_android/main/PostAdapter.kt
@@ -0,0 +1,32 @@
+package com.pcandriod.part_cross_android.main
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.pcandriod.part_cross_android.databinding.ItemPostBinding
+
+class PostAdapter(private val itemPost: ArrayList<PostItem>) :
+RecyclerView.Adapter<PostAdapter.PostViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
+        val binding = ItemPostBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PostViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
+        holder.setItem(itemPost[position].title, itemPost[position].content)
+        holder.binding.root.setOnClickListener {
+            Log.d("어댑터 클릭이벤트", "아이템 클릭!")
+        }
+    }
+
+    override fun getItemCount() = itemPost.size
+
+    class PostViewHolder(val binding: ItemPostBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun setItem(title: String, content: String) {
+            binding.tvTitle.text = title
+            binding.tvContent.text = content
+        }
+    }
+}
