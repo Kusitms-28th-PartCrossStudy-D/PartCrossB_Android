8a58a580331f04b6c856d71b67324f82e87414f1
arinming <jyandysys1@naver.com>
2023-09-09 16:56:28 +0900

    [feat] : 레트로핏 클라이언트 okhttp 추가

--- app/src/main/java/com/pcandriod/part_cross_android/api/RetrofitClient.kt
+++ app/src/main/java/com/pcandriod/part_cross_android/api/RetrofitClient.kt
@@ -2,10 +2,15 @@ package com.pcandriod.part_cross_android.api
 
 import com.google.gson.GsonBuilder
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 
 @Module
@@ -13,10 +18,36 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitClient {
     private const val BASE_URL = "https://kusitms.shop"
 
+    @Provides
+    @Singleton
+    fun okHttpClient(interceptor: Interceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            })
+            .build()
+    }
+
     private var gson = GsonBuilder().setLenient().create()
 
     private val client = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create(gson))
+        .client(okHttpClient(provideInterceptor()))
         .build()
+
+    fun getInstance(): Retrofit {
+        return client
+    }
+
+    @Provides
+    @Singleton
+    fun provideInterceptor(): Interceptor {
+        return Interceptor { chain ->
+            val request = chain.request()
+            val response = chain.proceed(request)
+            response
+        }
+    }
 }
--- app/src/main/java/com/pcandriod/part_cross_android/main/MainActivity.kt
+++ app/src/main/java/com/pcandriod/part_cross_android/main/MainActivity.kt
@@ -4,11 +4,19 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.pcandriod.part_cross_android.api.RetrofitService
 import com.pcandriod.part_cross_android.databinding.ActivityMainBinding
 import com.pcandriod.part_cross_android.detail.DetailActivity
+import com.pcandriod.part_cross_android.write.WriteActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    lateinit var articleService: RetrofitService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +29,7 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    fun initView() {
+    private fun initView() {
         val itemPost = ArrayList<PostItem>()
 
         itemPost.add(PostItem("제목1", "내용1"))
@@ -36,7 +44,7 @@ class MainActivity : AppCompatActivity() {
         itemPost.add(PostItem("제목10", "내용10"))
 
         binding.ibtnEdit.setOnClickListener {
-            val intent = Intent(this, DetailActivity::class.java)
+            val intent = Intent(this, WriteActivity::class.java)
             startActivity(intent)
         }
 
@@ -51,6 +59,15 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "${position+1} 번째 글 클릭!", Toast.LENGTH_SHORT).show()
             startActivity(intent)
         }
+    }
 
+    private fun getArticleList() {
+        lifecycleScope.launch {
+            val response = articleService.getPostList()
+            val statusCode = response
+
+            withContext(Dispatchers.Main) {
+            }
+        }
     }
 }
